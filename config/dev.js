a76a9acca09cff230061350d730a3ab6a647e231
shubiduba1212 <dldks1212@gmail.com>
2021-05-10 12:59:54 +0900

    비밀 정보 보호

---
+++ config/dev.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI:'mongodb+srv://jyejye:abcd1234@boiler-plate.fqfxc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+}
--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require("body-parser");
+const config = require('./config/key');
 
-const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://jyejye:abcd1234@boiler-plate.fqfxc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
+const { User } = require("./models/Users");
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}))
+
+//application/json
+app.use(bodyParser.json());
+
+const mongoose = require('mongoose');
+mongoose.connect(config.mongoURI, {
   useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB Connected'))
   .catch(() => console.log(err))
@@ -11,7 +21,21 @@ mongoose.connect('mongodb+srv://jyejye:abcd1234@boiler-plate.fqfxc.mongodb.net/m
 
 
 app.get('/', (req, res) => {
-  res.send('Hello World!~안녕하세요')
+  res.send('Hello World!~안녕하세요-2021년 5월입니다.')
+})
+
+app.post('/register', (req, res) => {
+  //회원 가입 할 때 필요한 정보들을 client에서 가져오면
+  //그것들을 데이터 베이스에 넣어준다.
+  
+  const user = new User(req.body) //bodyparser를 통해서 클라이언트에 들어온 사용자 정보를 서버에서 해석할 수 있도록 해준다.
+
+  user.save((err, userInfo) => {
+    if(err) return res.json({success:false, err})
+    return res.status(200).json({
+      success:true
+    })
+  }) //사용자 정보가 user모델에 저장된다.
 })
 
 app.listen(port, () => {
